dcf7a6e37c551f02e7c28b76a2225aea83971e8c
Bob Costas <finkzane@gmail.com>
2018-02-21 19:08:31 -0700

    Added constructor method headers

--- p5_Package/QueueClass.java
+++ p5_Package/QueueClass.java
@@ -1,4 +1,31 @@
 package p5_Package;
 
 public class QueueClass {
+    /**
+     * Queue data managed by IteratorClass object
+     */
+    IteratorClass queueData;
+
+    /**
+     * Default constructor.
+     */
+    public QueueClass()
+    {
+
+    }
+
+    /**
+     * Initialization constructor, constructs QueueClass with initial capacity
+     * @param setCapacity
+     */
+    public QueueClass( int setCapacity )
+    {
+
+    }
+
+    public QueueClass( QueueClass copied )
+    {
+        
+    }
+
 }
