bb598d1163db62482aa8df2dfec3ace6ee79647c
Bob Costas <finkzane@gmail.com>
2018-02-21 19:12:37 -0700

    Added constructor method headers

--- p5_Package/StackClass.java
+++ p5_Package/StackClass.java
@@ -1,4 +1,37 @@
 package p5_Package;
 
+import java.util.Stack;
+
 public class StackClass {
+
+    /**
+     *  stack data to be managed by iterator object
+     */
+    IteratorClass stackData;
+
+    /**
+     * Default constructor
+     */
+    public StackClass()
+    {
+
+    }
+
+    /**
+     * Initialization constructor, constructs StackClass with initial capacity
+     * @param setCapacity the capacity of the StackClass
+     */
+    public StackClass( int setCapacity )
+    {
+
+    }
+
+    /**
+     * Copies a stackClass object
+     * @param copied the StackClass object to be copied
+     */
+    public StackClass( StackClass copied )
+    {
+
+    }
 }
