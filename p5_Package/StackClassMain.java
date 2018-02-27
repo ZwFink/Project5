7118d5c7917d11d939e7a3164bf01cd86d6d33b5
Bob Costas <finkzane@gmail.com>
2018-02-26 19:24:15 -0700

    Added testing for StackClass and its methods

--- p5_Package/StackClassMain.java
+++ p5_Package/StackClassMain.java
@@ -3,6 +3,19 @@ package p5_Package;
 public class StackClassMain {
     public static void main( String[] args )
     {
-        
+        StackClass stack = new StackClass( 10 );
+        for( int index = 0; index < 20; index++ )
+        {
+            stack.push( index );
+        }
+        System.out.println(stack.stackData);
+        System.out.println( stack.peekTop() );
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.stackData);
+        StackClass sto = new StackClass( stack );
+        System.out.println(stack.stackData);
     }
 }
