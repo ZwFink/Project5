2d5c75900067b0f07ee791641e2ea0133d21255f
Bob Costas <finkzane@gmail.com>
2018-02-26 19:24:29 -0700

    Added testing for IteratorClass

--- p5_Package/IteratorClassMain.java
+++ p5_Package/IteratorClassMain.java
@@ -3,6 +3,31 @@ package p5_Package;
 public class IteratorClassMain {
     public static void main( String[] args )
     {
-        
+        IteratorClass ic = new IteratorClass( 5 );
+        final int NUMBER = 20;
+        for( int index = 0; index < NUMBER; index++ )
+        {
+            ic.insertPriorToIterator( index );
+            ic.moveNext();
+        }
+        System.out.println(ic);
+        ic.movePrevious();
+        System.out.println(ic);
+        ic.movePrevious();
+        System.out.println(ic);
+        ic.insertAfterIterator( 300 );
+        System.out.println(ic);
+        ic.movePrevious();
+        System.out.println(ic);
+        ic.movePrevious();
+        ic.movePrevious();
+        ic.movePrevious();
+        System.out.println(ic);
+        ic.removeAtCurrent();
+        ic.removeAtCurrent();
+        ic.removeAtCurrent();
+        ic.removeAtCurrent();
+        ic.removeAtCurrent();
+        System.out.println(ic);
     }
 }
